a441b38be1c2753bb4c17c85672f3d6143bb94f2
Mark Kaylor <mark.kaylor@strapi.io>
2021-03-04 10:36:00 +0100

    Fix api post request

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,28 @@
+export function getStrapiURL(path) {
+  return `${
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
+  }${path}`
+}
+
+// Helper to make GET requests to Strapi
+
+export async function fetchAPI(path, options = {}) {
+  const defaultOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  const mergedOptions = {
+    ...defaultOptions,
+    ...options,
+  }
+  const requestUrl = getStrapiURL(path)
+  const response = await fetch(requestUrl, mergedOptions)
+
+  if (!response.ok) {
+    console.error(response.statusText)
+    throw new Error(`An error occured please try again`)
+  }
+  const data = await response.json()
+  return data
+}
